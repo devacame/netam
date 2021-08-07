f63ea7e408467f238528e7b18bfc6ccea16cb3b8
coder38611 <54740236+coder38611@users.noreply.github.com>
2021-08-07 21:44:42 +0900

    refactor!: changed and removed custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      'lightBlue': '#548CA8',
+      'light': '#F9F9F9',
+      'dark': '#1A1A1D',
+      'darkBody': '#18181b',
+    }),
+    textColor: (theme) => theme('colors'),
+    textColor: {
+      'bluePrime': '#334257',
+      'blueSub': '#476072',
+      'baseColor': '#6b727b',
+      'lightBlue': '#548CA8',
+      'light': '#F9F9F9',
+    },
+    extend: {
+      backgroundImage: (theme) => ({
+        '404': "url('/404.svg')",
+        '500': "url('/500.svg')",
+      }),
+      fontFamily: {
+        body: ['Noto Sans KR'],
+      },
+    },
   },
   variants: {
     extend: {},
