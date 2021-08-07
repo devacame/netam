56e41c180481de3727767828c31a2c0f971c74d7
coder38611 <54740236+coder38611@users.noreply.github.com>
2021-08-07 21:52:57 +0900

    feat: added SEO for 404

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,24 @@
 import Link from 'next/link'
+import SEO from '@/components/SEO'
 
 export default function Error404() {
   const meta = {
     title: '404 Error | 404 에러',
   }
   return (
-    <div className='h-screen flex flex-col place-content-center items-center bg-404'>
-      <h1 className='text-9xl text-bluePrime'>404 에러</h1>
-      <h2 className='text-5xl text-gray-50 py-3'>
-        존재하지 않는 페이지입니다.
-      </h2>
-      <Link href='/'>
-        <a className='bg-gray-50 text-gray-800 hover:text-opacity-50 p-3 rounded-md'>
-          메인 화면으로 돌아가기
-        </a>
-      </Link>
+    <div>
+      <SEO meta={meta} />
+      <div className='h-screen flex flex-col place-content-center items-center bg-404'>
+        <h1 className='text-9xl text-bluePrime'>404 에러</h1>
+        <h2 className='text-5xl text-gray-50 py-3'>
+          존재하지 않는 페이지입니다.
+        </h2>
+        <Link href='/'>
+          <a className='bg-gray-50 text-gray-800 hover:text-opacity-50 p-3 rounded-md'>
+            메인 화면으로 돌아가기
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
