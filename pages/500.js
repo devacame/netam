fae0e475872032e9d43145757ec74d4f75639a3e
coder38611 <54740236+coder38611@users.noreply.github.com>
2021-08-07 20:46:49 +0900

    feat: added Error 500 page

---
+++ pages/500.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function Error404() {
+  return (
+    <div className='h-screen flex flex-col place-content-center items-center bg-500'>
+      <h1 className='text-9xl text-primary' style={{ filter: 'invert(1)' }}>
+        500 에러
+      </h1>
+      <h2 className='text-5xl text-light py-3'>존재하지 않는 페이지입니다.</h2>
+      <Link href='/'>
+        <a className='bg-eerie text-darkEerie hover:text-opacity-50 p-3 rounded-sm'>
+          메인 화면으로 돌아가기
+        </a>
+      </Link>
+    </div>
+  )
+}
