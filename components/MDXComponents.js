0d688efb6c94df82bc8d9c2449074c75e90c1d33
coder38611 <54740236+VESOC@users.noreply.github.com>
2021-09-04 22:24:07 +0900

    feat: added mdx support

---
+++ components/MDXComponents.js
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Anchor = (props) => {
+  const href = props.href
+  const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'))
+
+  if (isInternalLink) {
+    return (
+      <Link id='hi' href={href}>
+        <a className='text-xl' {...props}>
+          {props.children}
+        </a>
+      </Link>
+    )
+  }
+
+  return <a target='_blank' rel='noopener noreferrer' {...props} />
+}
+
+export const components = {
+  a: Anchor,
+  img: Image,
+}
---
+++ pages/blog/[slug].js
@@ -0,0 +1,38 @@
+import Container from '@/components/Container'
+import { getPosts, getPostbySlug } from '@/lib/markdown'
+import { MDXRemote } from 'next-mdx-remote'
+import { components } from '@/components/MDXComponents'
+
+export default function BlogPost({ frontmatter, markdown }) {
+  return (
+    <Container>
+      <h1>Post</h1>
+      <MDXRemote {...markdown} components={components} />
+    </Container>
+  )
+}
+
+export async function getStaticPaths() {
+  const posts = await getPosts()
+  const paths = posts.map((file) => ({
+    params: {
+      slug: file.replace('.mdx', ''),
+    },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const { frontmatter, markdown } = await getPostbySlug(slug)
+
+  return {
+    props: {
+      frontmatter,
+      markdown,
+    },
+  }
+}
