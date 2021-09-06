67579b5f3dc142bf09107f546b7e8734648ed633
VESOC <54740236+VESOC@users.noreply.github.com>
2021-09-06 13:40:42 +0900

    refactor: made nav items' font weight to bold

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,7 +8,7 @@ export default function NavBar() {
       <a href='#skip' className='skip-nav'>
         Skip to content
       </a>
-      <div className='flex flex-col'>
+      <div className='flex flex-col font-bold'>
         <Link href='/'>
           <a className='p-1 sm:p-4 text-bluePrime hover:text-blueSub dark:text-light dark:hover:text-opacity-30'>
             <h2>Home</h2>
