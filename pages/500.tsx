226a6daf135069a024b111f1b6be8f30e8c0fadd
VESOC <54740236+VESOC@users.noreply.github.com>
2021-10-23 22:03:41 +0900

    refactor: use tailwind to generate gradient background

--- pages/500.tsx
+++ pages/500.tsx
@@ -9,13 +9,8 @@ export default function Error500() {
     return (
         <div>
             <SEO meta={meta} />
-            <div className='h-screen flex flex-col place-content-center items-center bg-500'>
-                <h1
-                    className='text-9xl text-bluePrime'
-                    style={{ filter: 'invert(1)' }}
-                >
-                    500 에러
-                </h1>
+            <div className='h-screen flex flex-col place-content-center items-center bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'>
+                <h1 className='text-9xl text-bluePrime'>500 에러</h1>
                 <h2 className='text-5xl text-gray-50 py-3'>
                     존재하지 않는 페이지입니다.
                 </h2>
