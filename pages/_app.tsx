a4fc7212c252cb04b5e47fe77a6b8da342a84eaa
VESOC <54740236+VESOC@users.noreply.github.com>
2021-10-30 07:05:38 +0900

    fix: add auth provider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider defaultTheme='system' attribute='class'>
-            <Component {...pageProps} />
+            <SessionProvider session={pageProps.session}>
+                <Component {...pageProps} />
+            </SessionProvider>
         </ThemeProvider>
     )
 }
