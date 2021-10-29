0a641bfe7742c5248296eac37fa35ac71253878b
VESOC <54740236+VESOC@users.noreply.github.com>
2021-10-30 07:15:19 +0900

    refactor: moved base meta tags to document

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -11,10 +11,9 @@ export default function SEO({ meta }: PageProps) {
     return (
         <Head>
             <meta
-                name='viewport'
-                content='width=device-width, initial-scale=1'
+                name='robots'
+                content={meta?.nofollow ? 'nofollow, noindex' : 'follow, index'}
             />
-            <meta name='robots' content='follow, index' />
             <link
                 rel='canonical'
                 href={`${process.env.NEXT_PUBLIC_BASE_URL}${router.asPath}`}
@@ -31,9 +30,11 @@ export default function SEO({ meta }: PageProps) {
                     router.asPath.startsWith('/blog') ? 'article' : 'website'
                 }
             />
-            <meta property='og:site_name' content='VESOC' />
             <meta property='og:description' content={meta?.description} />
-            <meta property='og:locale' content='ko_KR' />
+            <meta
+                property='og:locale'
+                content={meta?.language ? meta.language : 'ko_KR'}
+            />
             <meta property='og:title' content={meta?.title} />
             {meta?.image && (
                 <meta
@@ -47,77 +48,6 @@ export default function SEO({ meta }: PageProps) {
                     content={new Date(meta?.date).toISOString()}
                 />
             )}
-
-            <link
-                rel='shortcut icon'
-                href='/icons/favicon.ico'
-                type='image/x-icon'
-            />
-            <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
-            <link
-                rel='apple-touch-icon'
-                sizes='57x57'
-                href='/icons/apple-touch-icon-57x57.png'
-            />
-            <link
-                rel='apple-touch-icon'
-                sizes='72x72'
-                href='/icons/apple-touch-icon-72x72.png'
-            />
-            <link
-                rel='apple-touch-icon'
-                sizes='76x76'
-                href='/icons/apple-touch-icon-76x76.png'
-            />
-            <link
-                rel='apple-touch-icon'
-                sizes='114x114'
-                href='/icons/apple-touch-icon-114x114.png'
-            />
-            <link
-                rel='apple-touch-icon'
-                sizes='120x120'
-                href='/icons/apple-touch-icon-120x120.png'
-            />
-            <link
-                rel='apple-touch-icon'
-                sizes='144x144'
-                href='/icons/apple-touch-icon-144x144.png'
-            />
-            <link
-                rel='apple-touch-icon'
-                sizes='152x152'
-                href='/icons/apple-touch-icon-152x152.png'
-            />
-            <link
-                rel='apple-touch-icon'
-                sizes='180x180'
-                href='/icons/apple-touch-icon-180x180.png'
-            />
-            <link
-                rel='icon'
-                type='image/png'
-                sizes='192x192'
-                href='/icons/android-icon-192x192.png'
-            />
-            <link
-                rel='icon'
-                type='image/png'
-                sizes='32x32'
-                href='/icons/favicon-32x32.png'
-            />
-            <link
-                rel='icon'
-                type='image/png'
-                sizes='96x96'
-                href='/icons/favicon-96x96.png'
-            />
-            <link
-                rel='icon'
-                type='image/png'
-                sizes='16x16'
-                href='/icons/favicon-16x16.png'
-            />
         </Head>
     )
 }
--- lib/types.ts
+++ lib/types.ts
@@ -1,12 +1,14 @@
 export interface MetaData {
-    title?: string,
-    description?: string,
-    image?: string,
-    date?: Date,
+    title?: string
+    description?: string
+    image?: string
+    date?: Date
+    nofollow?: boolean
+    language?: string
     category?: string[]
 }
 
-export interface BlogMeta extends MetaData{
-    readingTime: number,
+export interface BlogMeta extends MetaData {
+    readingTime: number
     slug: string
 }
