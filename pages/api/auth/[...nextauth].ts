67652ea4187a33dde1df1b299a206b0caa360899
VESOC <54740236+VESOC@users.noreply.github.com>
2021-11-13 20:27:55 +0900

    feat: use jwt instead of db for signing in

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,12 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import prisma from '@/lib/prisma'
+// import { PrismaAdapter } from '@next-auth/prisma-adapter'
+// import prisma from '@/lib/prisma'
 import protectAPI from '@/lib/apiProtection'
 
 export default protectAPI(
     NextAuth({
-        adapter: PrismaAdapter(prisma),
+        // adapter: PrismaAdapter(prisma),
         providers: [
             GithubProvider({
                 clientId: process.env.GITHUB_CLIENT_ID,
@@ -14,22 +14,23 @@ export default protectAPI(
             }),
         ],
         session: {
-            jwt: false,
+            jwt: true,
             maxAge: 1 * (24 * 60 * 60), // Session has a max age of 1 Day
         },
+        jwt: {
+            secret: process.env.JWT_SECRET,
+        },
         callbacks: {
-            // Only have signIn callback to prevent multi user signups - change according to your own use
-            async signIn({ user }) {
-                /* The owner check below only works because GitHub doesn't allow users to signup with identical usernames */
-                /* Implment secure user checks according to your use case */
-                const isSiteOwner =
-                    user.name?.toLowerCase() ===
-                    process.env.SITE_OWNER_NAME?.toLowerCase()
-                if (isSiteOwner) {
-                    return true
-                } else {
-                    return '/invaliduser'
+            async jwt({ token, user, isNewUser }) {
+                /* Prevent new user signups except owner + extra check with Github's unique username policy */
+                if (
+                    (isNewUser && token.name !== process.env.SITE_OWNER_NAME) ||
+                    !user
+                ) {
+                    return { token: null }
                 }
+                if (user) return token
+                return { token: null }
             },
         },
     })
