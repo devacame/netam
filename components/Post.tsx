375cb1c8b43dae1e18c43998eac995f42d4eac3e
VESOC <54740236+VESOC@users.noreply.github.com>
2021-11-17 17:03:45 +0900

    fix: fixed wrong key call

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,18 +1,24 @@
-import {Dispatch, SetStateAction} from 'react'
+import { Dispatch, SetStateAction } from 'react'
 
-export default function Post({ post, setSearchTerm }:{post: any, setSearchTerm: Dispatch<SetStateAction<string>>}) {
+export default function Post({
+    post,
+    setSearchTerm,
+}: {
+    post: any
+    setSearchTerm: Dispatch<SetStateAction<string>>
+}) {
     return (
         <div
             className='mt-3 w-full h-auto shadow-xl p-2'
             onClick={() => setSearchTerm('')}
         >
-            <a href={`/blog/${post.slug}`}>
+            <a href={`/blog/${post.id}`}>
                 <p className='text-xs'>{post.date}</p>
                 <h1 className='text-base'>{post.title}</h1>
                 <p className='text-sm'>{post.description}</p>
                 <ul className='flex flex-row list-none gap-x-2 text-purple-200'>
                     {post.category.slice(0, 3).map((category: string) => (
-                        <li key={category + post.slug}>#{category}</li>
+                        <li key={category + post.id}>#{category}</li>
                     ))}
                 </ul>
             </a>
