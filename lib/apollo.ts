ab1d2a5772f184d77230edd900eb6f253319e4b9
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-12 18:08:46 +0900

    refactor: uses env for uri

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const apolloClient = new ApolloClient({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: process.env.NEXT_PUBLIC_BASE_URL + '/api/graphql',
     cache: new InMemoryCache(),
 })
 
