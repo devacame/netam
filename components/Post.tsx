fb73b7fd5568f2f0c8ade93a1a123c914ac5b8c9
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-12 18:11:16 +0900

    refactor: now uses component for full posts only

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,41 +1,30 @@
-import { BlogMeta } from '@/lib/types'
+import { MetaData } from '@/lib/types'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Post({
-    post,
-    compact,
-}: {
-    post: Partial<BlogMeta>
-    compact: boolean
-}) {
+export default function Post({ post }: { post: MetaData }) {
     return (
-        <div
-            className={
-                compact
-                    ? 'mt-3 w-full h-auto shadow-xl p-2'
-                    : 'p-2 border-2 w-full'
-            }
-        >
+        <div className={'mt-3 w-full h-auto shadow-xl p-2'}>
             <Link href={`/blog/${post.id}`} passHref={true}>
                 <a className=''>
-                    {!compact && post.coverImage !== undefined && (
-                        <div className='relative w-full h-[20vh]'>
-                            <Image
-                                src={
-                                    process.env.NEXT_PUBLIC_IMAGE_LOADER_URL +
-                                    post.coverImage
-                                }
-                                alt={post.id + ' cover image'}
-                                layout='fill'
-                                objectFit='contain'
-                            />
-                        </div>
-                    )}
+                    <div className='relative w-full h-[20vh]'>
+                        <Image
+                            src={
+                                process.env.NEXT_PUBLIC_IMAGE_LOADER_URL! +
+                                post.coverImage
+                            }
+                            alt={post.id + ' cover image'}
+                            layout='fill'
+                            objectFit='contain'
+                        />
+                    </div>
                     <div className='p-1'>
                         <p className='text-xs'>{post.date}</p>
                         <h1 className='text-base'>{post.title}</h1>
-                        <p className='text-sm'>{post.description}</p>
+                        <p className='text-sm'>
+                            {post.series} |{' '}
+                            {post.description?.slice(0, 47) + '...'}
+                        </p>
                         <ul className='flex flex-row list-none gap-x-2 text-purple-400 dark:text-purple-200'>
                             {post
                                 .category!.slice(0, 3)
