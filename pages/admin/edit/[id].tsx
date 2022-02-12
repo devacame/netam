6ef4154897a1075781c2da774a92d7d3c1fb67ce
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-12 13:36:30 +0000

    fix: fix use of double quotes to single quotes

--- pages/admin/edit/[id].tsx
+++ pages/admin/edit/[id].tsx
@@ -1,15 +1,15 @@
-import Editor from "@/components/Editor"
-import { getPost } from "@/lib/PostData"
-import { PostData } from "@/lib/types"
-import { GetServerSideProps } from "next"
+import Editor from '@/components/Editor'
+import { getPost } from '@/lib/PostData'
+import { PostData } from '@/lib/types'
+import { GetServerSideProps } from 'next'
 
 export default function EditPost({ post }: { post: PostData }) {
-    return <Editor editorType="edit" post={post} />
+    return <Editor editorType='edit' post={post} />
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     const id = params!.id!
-    if (typeof id === "string" && id) {
+    if (typeof id === 'string' && id) {
         const { content, metadatas } = await getPost(id)
         return {
             props: {
@@ -22,6 +22,6 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     }
     return {
         props: {},
-        redirect: "/admin",
+        redirect: '/admin',
     }
 }
