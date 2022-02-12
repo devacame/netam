1f7399e51fe7e1dff8108d0fee5be160a4b3aefb
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-12 18:08:19 +0900

    update: uses separate component for search results

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,30 @@
-import Post from '@/components/Post'
 import { SearchResult } from '@/lib/types'
+import Link from 'next/link'
+
+function SearchResult({ result }: { result: SearchResult }) {
+    return (
+        <div className={'p-2 border-2 w-full'}>
+            <Link href={`/blog/${result.id}`} passHref={true}>
+                <a className=''>
+                    <div className='p-1'>
+                        <p className='text-xs'>{result.date}</p>
+                        <h1 className='text-base'>{result.title}</h1>
+                        <p className='text-sm'>{result.description}</p>
+                        <ul className='flex flex-row list-none gap-x-2 text-purple-400 dark:text-purple-200'>
+                            {result
+                                .category!.slice(0, 3)
+                                .map((category: string) => (
+                                    <li key={result.id + category}>
+                                        #{category}
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                </a>
+            </Link>
+        </div>
+    )
+}
 
 export default function SearchResults({
     results,
@@ -13,7 +38,7 @@ export default function SearchResults({
             <div className='p-7'>
                 <h2 className='mb-3'>결과 {results.length}개</h2>
                 {results.map((result, index) => (
-                    <Post key={index} post={result} compact={true} />
+                    <SearchResult key={index} result={result} />
                 ))}
             </div>
         </div>
