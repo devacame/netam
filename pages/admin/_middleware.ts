ce307499d2f21c146ee6a144b6f590a30dd702f9
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-12 14:02:50 +0000

    fix: use middleware support in next-auth

--- pages/admin/_middleware.ts
+++ pages/admin/_middleware.ts
@@ -1,11 +1,1 @@
-import { getToken } from 'next-auth/jwt'
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function middleware(req: NextRequest) {
-    const token = await getToken({ req, secret: process.env.JWT_SECRET! })
-    if (token) {
-        return NextResponse.next()
-    } else {
-        return NextResponse.redirect('/')
-    }
-}
+export { default } from "next-auth/middleware"
