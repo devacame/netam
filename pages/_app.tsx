344a585f5fb4d456b61840ab98ecbaa1acc879ce
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-16 00:25:40 +0900

    fix: fix no viewport in document error for nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,19 @@ import { SessionProvider } from 'next-auth/react'
 import { ApolloProvider } from '@apollo/client'
 import apolloClient from '@/lib/apollo'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider defaultTheme='system' attribute='class'>
             <SessionProvider session={pageProps.session}>
                 <ApolloProvider client={apolloClient}>
+                    <Head>
+                        <meta
+                            name='viewport'
+                            content='width=device-width, initial-scale=1.0'
+                        />
+                    </Head>
                     <Component {...pageProps} />
                 </ApolloProvider>
             </SessionProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,7 @@ class MyDocument extends Document {
             <Html lang='ko' className='scrollbar-hidden'>
                 <Head>
                     <meta charSet='utf-8' />
-                    <meta
-                        name='viewport'
-                        content='width=device-width, initial-scale=1.0'
-                    />
+                    
                     <link
                         rel='shortcut icon'
                         href='/icons/favicon.ico'
