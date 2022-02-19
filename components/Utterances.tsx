1bae196ea0c0fed19195f64ee9ab46a86d57c296
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-19 15:01:14 +0000

    refactor: use localstorage to get theme data

--- components/Utterances.tsx
+++ components/Utterances.tsx
@@ -1,20 +1,20 @@
 import { useEffect } from 'react'
-import { useTheme } from 'next-themes'
 
 export default function Utterances() {
-    const { resolvedTheme } = useTheme()
-    const currentColor =
-        resolvedTheme === 'dark' ? 'github-dark' : 'github-light'
     useEffect(() => {
+        const theme = localStorage.getItem('theme')
         const elem = document.getElementById('utterances')
         const scriptElem = document.createElement('script')
         scriptElem.src = 'https://utteranc.es/client.js'
         scriptElem.async = true
         scriptElem.setAttribute('repo', 'VESOC/vesoc-blog-comments')
         scriptElem.setAttribute('issue-term', 'title')
-        scriptElem.setAttribute('theme', currentColor)
+        scriptElem.setAttribute(
+            'theme',
+            theme === 'dark' ? 'github-dark' : 'github-light'
+        )
         scriptElem.crossOrigin = 'anonymous'
         elem!.appendChild(scriptElem)
-    })
+    }, [])
     return <section id='utterances' className='w-full' />
 }
