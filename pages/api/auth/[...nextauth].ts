546ed5c04464972cfad747ed5f0aa61917a57f5b
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-19 23:11:36 +0900

    deployfix: use auth0 for temp to replace github

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,32 +1,33 @@
 import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
+// import GithubProvider from 'next-auth/providers/github'
+import Auth0Provider from 'next-auth/providers/auth0'
 import protectAPI from '@/lib/apiProtection'
 
-// export default protectAPI(
-//     NextAuth({
-//         providers: [
-//             GithubProvider({
-//                 clientId: process.env.GITHUB_CLIENT_ID,
-//                 clientSecret: process.env.GITHUB_CLIENT_SECRET,
-//             }),
-//         ],
-//         secret: process.env.NEXTAUTH_SECRET,
-//         jwt: {
-//             maxAge: 1 * (24 * 60 * 60), // Token has a max age of 1 Day
-//         },
-//         theme: {
-//             colorScheme: 'auto', // "auto" | "dark" | "light"
-//             logo: '/icons/LOGO.png', // Absolute URL to image
-//         },
-//         callbacks: {
-//             async jwt({ token, user, isNewUser }) {
-//                 /* Prevent new user signups except owner + extra check with Github's unique username policy */
-//                 if (isNewUser && token.name !== process.env.SITE_OWNER_NAME) {
-//                     return { token: null }
-//                 }
-//                 if (user) return token
-//                 return { token: null }
-//             },
-//         },
-//     })
-// )
+export default protectAPI(
+    NextAuth({
+        providers: [
+            Auth0Provider({
+                clientId: process.env.AUTH_CLIENT_ID!,
+                clientSecret: process.env.AUTH_CLIENT_SECRET!,
+            }),
+        ],
+        secret: process.env.NEXTAUTH_SECRET,
+        jwt: {
+            maxAge: 1 * (24 * 60 * 60), // Token has a max age of 1 Day
+        },
+        theme: {
+            colorScheme: 'auto', // "auto" | "dark" | "light"
+            logo: '/icons/LOGO.png', // Absolute URL to image
+        },
+        callbacks: {
+            async jwt({ token, user, isNewUser }) {
+                /* Prevent new user signups except owner + extra check with Github's unique username policy */
+                if (isNewUser && token.name !== process.env.SITE_OWNER_NAME) {
+                    return { token: null }
+                }
+                if (user) return token
+                return { token: null }
+            },
+        },
+    })
+)
