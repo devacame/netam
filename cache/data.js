cd1b9fa5af3daca0ec0ba771d276afc8f9467cb8
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-19 22:27:36 +0900

    fix: add example data in cache

--- cache/data.js
+++ cache/data.js
@@ -1,2 +1,14 @@
-const postData = []
+const postData = [
+    {
+        __typename: 'Post',
+        id: 'netamlog1',
+        title: 'NETAMLOG[0] Hello Internet',
+        description: '블로그 프로젝트 1번째 글',
+        date: '2021-11-20',
+        category: ['블로그', '시작', 'Netamlog0'],
+        series: 'NETAMLOG',
+        coverImage: '/log4-nav.png',
+        readingTime: 6,
+    },
+]
 export default postData
