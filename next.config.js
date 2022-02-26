cd0f7f4e47ad52b6608c56bfc527a20db6be930b
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-26 00:59:52 +0000

    feat: add security headers in next config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,62 @@ module.exports = {
     images: {
         domains: ['res.cloudinary.com'],
     },
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: '/:path*',
+                headers: securityHeaders,
+            },
+        ]
+    },
 }
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src 'self';
+  style-src 'self';
+  img-src *.cloudinary.com;
+  media-src 'none';
+  connect-src 'self' *.cloudinary.com;
+  font-src 'self';
+`
+const securityHeaders = [
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\n/g, ''),
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+    {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+    // Opt-out of Google FLoC: https://amifloced.org/
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+    },
+]
