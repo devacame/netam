3f496d224f6b3812fc0f3999e03661e53b320fc1
VESOC <54740236+VESOC@users.noreply.github.com>
2022-02-26 03:22:39 +0000

    fix: load cloudinary widget before editor is loaded

--- pages/admin/create.tsx
+++ pages/admin/create.tsx
@@ -1,5 +1,14 @@
 import Editor from '@/components/Editor'
+import Script from 'next/script'
 
 export default function Create() {
-    return <Editor editorType='new' />
+    return (
+        <div>
+            <Script
+                src='https://upload-widget.cloudinary.com/global/all.js'
+                strategy='afterInteractive'
+            />
+            <Editor editorType='new' />
+        </div>
+    )
 }
--- pages/admin/edit/[id].tsx
+++ pages/admin/edit/[id].tsx
@@ -2,9 +2,18 @@ import Editor from '@/components/Editor'
 import { getPost } from '@/lib/PostData'
 import { PostData } from '@/lib/types'
 import { GetServerSideProps } from 'next'
+import Script from 'next/script'
 
 export default function EditPost({ post }: { post: PostData }) {
-    return <Editor editorType='edit' post={post} />
+    return (
+        <div>
+            <Script
+                src='https://upload-widget.cloudinary.com/global/all.js'
+                strategy='afterInteractive'
+            />
+            <Editor editorType='edit' post={post} />
+        </div>
+    )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
