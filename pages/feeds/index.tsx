34e0ce085c6f22bc4342115f32d91d3383837d13
VESOC <54740236+VESOC@users.noreply.github.com>
2022-05-27 20:38:06 +0900

    fix: add SEO to feeds page

--- pages/feeds/index.tsx
+++ pages/feeds/index.tsx
@@ -1,10 +1,19 @@
 import { GetStaticProps } from 'next'
 import generateCacheNFeeds from '@/lib/site-data'
 import Link from 'next/link'
+import SEO from '@/components/SEO'
+import { SEOData } from '@/lib/types'
 
 export default function Index() {
+    const meta: SEOData = {
+        title: 'Feeds',
+        description: 'Feed Page including XML, JSON, and Atom',
+        date: '2022-01-01',
+        coverImage: 'icons/LOGO.png',
+    }
     return (
         <div className='w-screen h-screen flex flex-col bg-gray-100 dark:bg-darkBody'>
+            <SEO meta={meta} />
             <div className='flex-1 flex flex-col justify-center items-center'>
                 <Link href='/feeds/feed.xml' passHref>
                     <a className='text-3xl text-sky-100'>Feed XML</a>
